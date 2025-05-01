ed36ac395a883232e9d03f8e40c1eb18a038151c
Jonnn <145574019+jon012906@users.noreply.github.com>
2025-05-01 19:41:14 +0700

    Upload lbv dataset, change edit csv in nb.

---
+++ librivox-indonesia/languages.py
@@ -0,0 +1,11 @@
+# We use ISO639-3 language code (https://iso639-3.sil.org/code_tables/639/data)
+LANGUAGES = {
+    'ace': 'Acehnese',
+    'bal': 'Balinese',
+    'bug': 'Bugisnese',
+    'ind': 'Indonesian',
+    'min': 'Minangkabau',
+    'jav': 'Javanese',
+    'sun': 'Sundanese',
+    '_all_': 'All'
+}
---
+++ librivox-indonesia/release_stats.py
@@ -0,0 +1,25 @@
+STATS = {
+    "name": "Librivox-Indonesia",
+    "version": "1.0.0",
+    "date": "2022-09-04",
+    "locales": {
+        "ace": {'reportedSentences': 149, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+                'avgDurationSecs': 1, 'totalHrs': 1},
+        "bal": {'reportedSentences': 175, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+                'avgDurationSecs': 1, 'totalHrs': 1},
+        "bug": {'reportedSentences': 142, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+                'avgDurationSecs': 1, 'totalHrs': 1},
+        "ind": {'reportedSentences': 6238, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+               'avgDurationSecs': 1, 'totalHrs': 1},
+        "min": {'reportedSentences': 156, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+                'avgDurationSecs': 1, 'totalHrs': 1},
+        "jav": {'reportedSentences': 801, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+                'avgDurationSecs': 1, 'totalHrs': 1},
+        "sun": {'reportedSentences': 154, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+                'avgDurationSecs': 1, 'totalHrs': 1},
+        "_all_": {'reportedSentences': 7815, 'duration': 1, 'clips': 1, 'users': 416, 'size': 1,
+                'avgDurationSecs': 1, 'totalHrs': 1},
+    },
+    'totalDuration': 1, 'totalHrs': 1
+}
+
